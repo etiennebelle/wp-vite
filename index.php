a77d595f47a5ce79afac2f587903e52777158a23
Etienne Bellé <bl.etienne7@gmail.com>
2024-10-24 18:08:28 +0200

    create public folder for static assets

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 
   <main>
-    <h1 class="headline headline-xl">WordPress Starter theme using Vite & Sass</h1>
+    <h1 class="headline headline-xl">WP Starter theme using Vite & Sass</h1>
   </main>
 
 <?php get_footer(); ?>
