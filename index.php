50473eb1c2e2dfe3884f9f030d008c5c07253ded
Etienne Bellé <bl.etienne7@gmail.com>
2024-10-24 12:01:36 +0200

    initial commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 
   <main>
-    <h1 id="title">WordPress Starter theme using Vite & Sass</h1>
+    <h1 class="headline headline-xl">WP Starter theme using Vite & Sass</h1>
   </main>
 
 <?php get_footer(); ?>
